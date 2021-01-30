6fab77147de50a4909a7a5054097a318b92970a9
eyapici <eyapici@ebay.com>
2021-01-30 17:16:11 +0300

    added 1.5 project

---
+++ 1.5/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigurationProperties
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+
+    @NotNull
+    @Value("${foo}")
+    private String foo;
+
+    @GetMapping(value = "/a")
+    private String sout() {
+        return foo;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
